d1f8d7116b41c4c434f87e8cb5dd4f2ced388655
TechStudent10 <76978184+TechStudent10@users.noreply.github.com>
2026-01-19 12:06:10 -0700

    Revert "Revert "Tyler branch""

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -154,4 +154,17 @@ public class Constants {
         public static final double rollerMaxAcceleration = 40;
         public static final double rollerMaxVelocity = 100; // rps
     }
+
+    public static class ClimbConstants
+    {
+        public static final int kClimbMotorID = 32;
+
+        public static final double kClimbMaxVelocity = 90; // rps
+        public static final int kClimbCURRENT_LIMIT = 30;   
+        public static final double kClimb_SPEED = .3; 
+
+
+        public static final int kClimbGearRatio = 9 / 1;
+
+    }
 }

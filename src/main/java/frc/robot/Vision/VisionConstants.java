ece68c5e850a555a2e79024b5c941db6a6d1f89c
Joel Amaldas <joel.amaldas@gmail.com>
2026-02-02 11:50:34 -0700

    camera pos changes

--- src/main/java/frc/robot/Vision/VisionConstants.java
+++ src/main/java/frc/robot/Vision/VisionConstants.java
@@ -51,11 +51,11 @@ public class VisionConstants {
 
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 = new Transform3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0));
-    public static Transform3d robotToCamera1 = new Transform3d(Units.inchesToMeters(9.287),
-            Units.inchesToMeters(-10.9704),
-            Units.inchesToMeters(7.9167),
-            new Rotation3d(0.0, Units.degreesToRadians(-15), Units.degreesToRadians(30)));
+    public static Transform3d robotToCamera1 = new Transform3d(31.75, 31.75, 0.0, new Rotation3d(0.0, 0.0, 0.0));
+    public static Transform3d robotToCamera0 = new Transform3d(Units.inchesToMeters(12.5),
+            Units.inchesToMeters(-12.5),
+            Units.inchesToMeters(6.2992),
+            new Rotation3d(0.0, Units.degreesToRadians(15), Units.degreesToRadians(0)));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;

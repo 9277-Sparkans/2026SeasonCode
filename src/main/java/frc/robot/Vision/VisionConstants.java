a1b96268c7e9088790540e4847f46b30162a3553
Joel A <153974259+yoi20@users.noreply.github.com>
2026-02-02 11:54:18 -0700

    Update robotToCamera0 transform parameters

--- src/main/java/frc/robot/Vision/VisionConstants.java
+++ src/main/java/frc/robot/Vision/VisionConstants.java
@@ -53,9 +53,9 @@ public class VisionConstants {
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
     public static Transform3d robotToCamera0 =
-        new Transform3d(Units.inchesToMeters(10.5), Units.inchesToMeters(11.5),
-            Units.inchesToMeters(6.5),
-            new Rotation3d(0.0, Units.degreesToRadians(-15), Units.degreesToRadians(-30)));
+        new Transform3d(Units.inchesToMeters(12.5), Units.inchesToMeters(12.5),
+            Units.inchesToMeters(6.2992),
+            new Rotation3d(0.0, Units.degreesToRadians(15), Units.degreesToRadians(0)));
     public static Transform3d robotToCamera1 =
         new Transform3d(Units.inchesToMeters(9.287), Units.inchesToMeters(-10.9704),
             Units.inchesToMeters(7.9167),

d7cb3802ed9ef26faf83c61f52e50a5108ee75db
DanielR723 <danielr723@stemia.ca>
2026-02-28 22:00:04 -0700

    Filled in RED_HUB_(X/Y)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -360,8 +360,8 @@ public class Constants {
     public static class FieldConstants {
         public static final double BLUE_HUB_X = 4.625594; // meters
         public static final double BLUE_HUB_Y = 4.034536; // meters
-        public static final double RED_HUB_X = 123123.0; // meters 
-        public static final double RED_HUB_Y = 123123.0; // meters
+        public static final double RED_HUB_X = 11.915394; // meters 
+        public static final double RED_HUB_Y = 4.034536; // meters
         public static final edu.wpi.first.math.geometry.Translation3d HUB_BLUE = new edu.wpi.first.math.geometry.Translation3d(
                 BLUE_HUB_X, BLUE_HUB_Y, 1.4); // 1.4m height approx
         public static final edu.wpi.first.math.geometry.Translation3d HUB_RED = new edu.wpi.first.math.geometry.Translation3d(

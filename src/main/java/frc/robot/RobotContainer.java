ec8dfb8129850c899ed80e4946cd08631ec1303c
AnuT003 <anu.tomo003@gmail.com>
2026-03-06 16:21:06 -0700

    o

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -482,6 +482,6 @@ public class RobotContainer {
 
         public Command getAutonomousCommand() {
                 // return autoChooser.getSelected();
-                return new PathPlannerAuto("henockauto");
+                return new PathPlannerAuto("oneintakes3climb");
         }
 }
